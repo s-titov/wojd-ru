bc800a6b765fa33ce47a11117c8e59529d59f2cd
Sergey Titov <titov.nubo@gmail.com>
2025-05-31 21:23:33 +0500

    Move txt translations

--- tools/jdt/cmd/transfer_txt_translate.go
+++ tools/jdt/cmd/transfer_txt_translate.go
@@ -10,12 +10,26 @@ import (
 
 type FileName string
 type TranslationKey string
+type Translations map[FileName]map[TranslationKey]string
 
 func main() {
 	sourceDir := "patch\\cn\\~Ru_Patch_P\\ZhuxianClient\\gamedata\\client\\FormatString"
-	//targetDir := "patch\\tw\\~Ru_Patch_P\\ZhuxianClient\\gamedata\\client\\FormatString"
+	targetDir := "patch\\tw\\~Ru_Patch_P\\ZhuxianClient\\gamedata\\client\\FormatString"
 
-	// TODO: prepare translations map
+	translations, err := prepareTxtTranslations(sourceDir)
+	if err != nil {
+		panic(err)
+	}
+
+	err = translateTxtTarget(targetDir, translations)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully translated")
+}
+
+func prepareTxtTranslations(sourceDir string) (Translations, error) {
 	translations := map[FileName]map[TranslationKey]string{}
 	err := filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -35,10 +49,10 @@ func main() {
 		return nil
 	})
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	fmt.Println(translations)
+	return translations, nil
 }
 
 func parseTranslationFile(filePath string) (map[TranslationKey]string, error) {
@@ -73,4 +87,85 @@ func parseTranslationFile(filePath string) (map[TranslationKey]string, error) {
 	}
 
 	return result, nil
+}
+
+func translateTxtTarget(targetDir string, translations Translations) error {
+	var fileName FileName
+	var file *os.File
+	var tempFile *os.File
+
+	err := filepath.Walk(targetDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".txt" {
+			fileName = FileName(strings.TrimPrefix(path, targetDir))
+
+			// if we have translations for this file
+			if fileTranslations, ok := translations[fileName]; ok {
+				// match to translate
+				file, err = os.Open(path)
+				if err != nil {
+					return err
+				}
+				defer func() { _ = file.Close() }()
+
+				targetPath := file.Name()
+				tempPath := file.Name() + ".tmp"
+				tempFile, err = os.Create(tempPath)
+				if err != nil {
+					return err
+				}
+				defer func() { _ = tempFile.Close() }()
+
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					line := scanner.Text()
+
+					// transfer as is
+					line = strings.TrimSpace(line)
+					if line == "" || strings.HasPrefix(line, "#") {
+						_, err = tempFile.WriteString(line)
+						if err != nil {
+							return err
+						}
+
+						continue
+					}
+
+					// mappings translate
+					parts := strings.SplitN(line, "=", 2)
+					if len(parts) == 2 {
+						key := TranslationKey(strings.TrimSpace(parts[0]))
+						value := strings.TrimSpace(parts[1]) + " //TODO: Translate"
+						if translation, ok := fileTranslations[key]; ok {
+							value = translation
+						}
+
+						newLine := fmt.Sprintf("%s = %s\n", key, value)
+						_, err = tempFile.WriteString(newLine)
+					}
+				}
+
+				err = tempFile.Close()
+				if err != nil {
+					return err
+				}
+				err = file.Close()
+				if err != nil {
+					return err
+				}
+
+				// rewrite original file
+				err = os.Rename(tempPath, targetPath)
+				if err != nil {
+					return err
+				}
+			}
+
+		}
+		return nil
+	})
+
+	return err
 }
