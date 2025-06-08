2dc1bd6a8f19b7feb6e008b23b2a4384a95987ae
Sergey Titov <titov.nubo@gmail.com>
2025-06-09 02:03:52 +0500

    Add tag checker script

---
+++ tools/jdt/cmd/tag_checker.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"slices"
+)
+
+func main() {
+	srcCsv := "patch/tw/Locres/Game.csv"
+	err := checkTag(srcCsv)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func checkTag(srcCsv string) error {
+	file, err := os.Open(srcCsv)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = file.Close() }()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1
+
+	// skip first row
+	_, err = reader.Read()
+	if err != nil {
+		return err
+	}
+
+	var record []string
+	for {
+		record, err = reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if len(record) < 2 {
+			continue
+		}
+		key := record[0]
+		source := record[1]
+		target := record[2]
+
+		if target != "" {
+			re := regexp.MustCompile(`\(@@Link:[0-9]*\)`)
+			sourceMatches := re.FindAllString(source, -1)
+
+			// если в source есть Link
+			if len(sourceMatches) > 0 {
+				targetMatches := re.FindAllString(target, -1)
+
+				if len(sourceMatches) != len(targetMatches) {
+					fmt.Println(fmt.Sprintf("qty not matched: %s", key))
+				}
+
+				// сверяем, что линки матчатся с таргетом
+				for _, sourceMatch := range sourceMatches {
+					if !slices.Contains(targetMatches, sourceMatch) {
+						fmt.Println(fmt.Sprintf("Links not match: %s", key))
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
